6e427414ffe53a559f05a3e4c5960ffc90f15381
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-04-11 15:14:34 +0530

    Section One - > First Name , Last Name , email implemented

--- HotelManaza(ComplexScreensBuilding)/Registration.swift
+++ HotelManaza(ComplexScreensBuilding)/Registration.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+struct Registration {
+    var firstName : String
+    var lastName : String
+    var email : String
+    
+    
+    var checkInDate : Date
+    var checkOutDate : Date
+    
+    var numberOfAdults : Int
+    var numberOfChildren : Int
+    
+    var wifi : Bool
+    
+    var roomType : RoomType
+}
+
+struct RoomType: Equatable{
+    static var all: [RoomType]{
+        return [RoomType(id: 0, name: "Two Queens", shortName: "2Q", price: 179),
+                RoomType(id: 1, name: "One King", shortName: "K", price: 209),
+                RoomType(id: 2, name: "Penthouse Suite", shortName: "PHS", price: 309)
+        ]
+    }
+    
+    var id: Int
+    var name: String
+    var shortName: String
+    var price: Int
+    
+    static func ==(lhs: RoomType , rhs: RoomType) -> Bool{
+        return lhs.id == rhs.id
+    }
+}
